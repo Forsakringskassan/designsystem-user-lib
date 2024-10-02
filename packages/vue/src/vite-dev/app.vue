f95f8c38f297a7d0137f6b363e27d84e70a5c19b
Martinsson Daniel (66129690) <daniel.martinsson@forsakringskassan.se>
2024-10-02 15:36:17 +0200

    add vite-dev

---
+++ packages/vue/src/vite-dev/app.vue
@@ -0,0 +1,21 @@
+<template>
+    <c-rating v-model="valtAntal">Hur många köttbullar?</c-rating>
+    <p>Du har valt {{ valtAntal }} köttbull(ar).</p>
+    <c-rating v-model="valtAntal2">Hur många köttbullar2?</c-rating>
+    <p>Du har valt {{ valtAntal2 }} köttbull(ar).</p>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import { CRating } from "../components";
+
+export default defineComponent({
+    components: { CRating },
+    data() {
+        return {
+            valtAntal: 3,
+            valtAntal2: 3,
+        };
+    },
+});
+</script>
