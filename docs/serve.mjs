a90d223da39f791b22cb3fd9502b70f8f72b6921
David Sveningsson <david.sveningsson@forsakringskassan.se>
2024-10-02 11:24:55 +0200

    build: make `npm start` do something useful

---
+++ docs/serve.mjs
@@ -0,0 +1,19 @@
+import express from "express";
+
+const defaultPort = 8080;
+const port = process.env.HTTP_PORT
+    ? parseInt(process.env.HTTP_PORT, 10)
+    : defaultPort;
+const folders = process.argv.slice(2);
+
+const app = express();
+
+for (const folder of folders) {
+    app.use("/", express.static(folder));
+}
+
+const server = app.listen(port, "::", () => {
+    const addr = server.address();
+    /* eslint-disable-next-line no-console -- expected to log */
+    console.log(`Server started at http://localhost:${addr.port}`);
+});
