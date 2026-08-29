ff29e1dc932581fb1145d59dcff7a6eb2be4f531
Försäkringskassan <sbutvecklinggithub@forsakringskassan.se>
2026-08-29 17:40:29 +0000

    chore(release): [skip ci] deploy v1.5.14 documentation

--- v1.5.14/components/fkds/FooExample-d4f03a.js
+++ v1.5.14/components/fkds/FooExample-d4f03a.js
@@ -27,7 +27,7 @@ function setup(options) {
   };
 }
 
-// virtual-entry:virtual:docs/components/fkds/FooExample.vue:FooExample-9b61d8.js
+// virtual-entry:virtual:docs/components/fkds/FooExample.vue:FooExample-d4f03a.js
 import { defineComponent } from "vue";
 import { DateFormat, FDate, Weekday } from "@fkui/date";
 import { FDatepickerField, FSelectField, FTooltip } from "@fkui/vue";
@@ -211,7 +211,7 @@ function render(_ctx, _cache, $props, $setup, $data, $options) {
 exampleComponent.render = render;
 setup({
   rootComponent: exampleComponent,
-  selector: "#example-9b61d8"
+  selector: "#example-d4f03a"
 });
 export {
   render
